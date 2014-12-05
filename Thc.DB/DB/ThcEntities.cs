77359a4072b7e25939ebb4a88ff94f54e487b345
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2014-12-04 23:07:27 -0500

    ThcMvcVs.C
    Se agregaron packetes de Unity & EntityFramework. :imp:

---
+++ Thc.DB/DB/ThcEntities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+using Thc.Models.Models;
+using Thc.DB.Map;
+
+namespace Thc.DB.DB
+{
+    public class ThcEntities : DbContext
+    {
+
+        public virtual IDbSet<Cliente> Clientes { get; set; }
+        public virtual IDbSet<Proveedor> Proveedores { get; set; }
+        public virtual IDbSet<Ciudad> Ciudadess { get; set; }
+        public virtual IDbSet<Auto> Autos { get; set; }
+        public virtual IDbSet<Estado> Estados { get; set; }
+        public virtual IDbSet<Unidad> Unidades { get; set; }
+        public virtual IDbSet<Alquiler> Alquileres { get; set; }
+        public virtual IDbSet<Conductor> Conductores { get; set; }
+        public virtual IDbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<DemoEntities>(null);
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add<Cliente>(new ClienteConfiguration());
+            modelBuilder.Configurations.Add<Proveedor>(new ProveedorConfiguration());
+            modelBuilder.Configurations.Add<Auto>(new AutoConfiguration());
+            modelBuilder.Configurations.Add<Alquiler>(new AlquilerConfiguration());
+        }
+    }
+}
